d14bb050ca97ab3e7ac8661f8e51e29ded87d6f3
LeBwa <xakep.dead@gmail.com>
2020-06-17 21:54:32 +0300

    fix issue with deleting device

--- NTNN/scripts/create_LogRegisteredDeviceInfoTable.sql
+++ NTNN/scripts/create_LogRegisteredDeviceInfoTable.sql
@@ -1,7 +1,7 @@
 USE [NTNN]
 CREATE TABLE [RegisteredDeviceInfo](
 	LogPK int PRIMARY KEY NOT NULL IDENTITY(1,1),
-	RegisteredDeviceFK int not null Foreign key references RegisteredDevices(RegisteredDevicePK),
+	RegisteredDeviceFK int not null,
 	LogJSON nvarchar(max) not null,
 	ClassType nvarchar(100),
 	LogDate datetime2 not null
